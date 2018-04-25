7ef5710dfcffc601d1085fbfb9125788d9edba5e
Zilin Chen <zilinchen@Zilins-MacBook.local>
2018-04-24 21:49:32 -0400

    GreedyAnnealing

---
+++ src/main/java/Annealing.java
@@ -0,0 +1,183 @@
+//Implement the Simulated Annealing Algorithm
+//Zilin Chen
+//4/24/2018
+import java.lang.Math;
+
+
+public class Annealing {
+
+	//instructor
+	public Annealing(){
+		System.out.println("This is the Simulated Annealing Algorithm: \n");
+	}
+
+	public static long Annealing(long[] arr, int iteration){
+		long[] subset = generateRandomSubset(arr);
+		long smallest_r = getSmallestResidue(arr, subset, iteration);
+		return smallest_r;
+	}
+
+	//generate list of 100 random numbers from 1 to 10^12
+	public static long[] generateRandomList(int num){
+		long[] list = new long[num];
+		long leftLimit = 1;
+		long rightLimit = (long) Math.pow(10,12);
+		// System.out.print(leftLimit+(rightLimit-leftLimit)*Math.random());
+		for (int i=0; i<num ; i++) {
+			list[i] = (long)(leftLimit + Math.random()*(rightLimit-leftLimit));
+			// System.out.print(list[i]+"\n");
+		}
+		return list;
+	}
+
+	//print out the list
+	public static void printList(long[] list){
+		for (int i=0; i<list.length ;i++ ) {
+			System.out.print(i + ": " + list[i] + "\n");
+		}
+		System.out.println();
+	}
+
+	
+	//Choose a random subset of S as the current subset.
+	public static long[] generateRandomSubset(long[] originalList){
+		double probability = 0.5;
+		long[] subset = new long[originalList.length];
+		//every element has 50% change of being added to the sublist
+		for (int i=0;i<originalList.length ;i++ ) {
+			if (Math.random() >= probability) {
+				subset[i] = originalList[i];
+			}
+		}
+		return subset;
+	}
+
+	//generate neighbor set
+	public static long[] generateRandomNeighbor(long[] originalList, long[] subset_s){
+		//let subset_t be a copy of subset_s
+		long[] subset_t = new long[subset_s.length];
+		for (int i=0; i<subset_s.length; i++) {
+			subset_t[i] = subset_s[i];
+		}
+
+		//generate two random indices
+		int index_i = 0;
+		int index_j = 0;
+
+		while (index_i == 0 || index_j == 0 || index_i == index_j){
+			index_i = 1 + (int)(Math.random()*(subset_s.length-1));
+			index_j = 1 + (int)(Math.random()*(subset_s.length-1));
+		}
+
+		// System.out.print(index_i +","+ index_j+"\n");
+		// System.out.print(subset_s[index_i] + "," + subset_s[index_j] + "\n");
+
+		//if xi is in s, remove it from t. Otherwise, add xi to t.
+		if (subset_s[index_i] != 0) {
+			subset_t[index_i] = 0;
+		} else { subset_t[index_i] = originalList[index_i]; }
+
+		//if xj is in s, then with probability 0.5, remove it from t. 
+		//If xj is not in s, then with probability 0.5, add xj to t.
+		if (subset_s[index_j] != 0) {
+			if (Math.random() >= 0.5) {
+				subset_t[index_j] = 0;
+			} else {
+				if (Math.random() >= 0.5){
+					subset_t[index_j] = originalList[index_j];
+				}
+			}
+		}
+
+		return subset_t;
+	}
+
+	public static long getResidue(long[] list){
+		long sum = 0;
+		long target = (long)(25*Math.pow(10,12));
+		for (int i=0; i<list.length; i++) {
+			sum += list[i];
+		}
+		long residue = target - sum;
+		return residue;
+	}
+
+	
+	public static long getSmallestResidue(long[] originalList, long[] sublist, int iteration){
+		long current_r = 0;
+		long neighbor_r = 0;
+		long smallest_r = (long)Math.pow(10,15); //give it a big number
+		long[] current = sublist;
+
+		for (int i=0; i<iteration; i++) {
+			//save the smallest residue of all current subsets
+			if (getResidue(current) < smallest_r) {
+				smallest_r = getResidue(current);
+			}
+			long[] neighbor = generateRandomNeighbor(originalList, current);
+
+			//If the neighbor has smaller residue, then make it the current subset.
+			if (getResidue(neighbor) < getResidue(current)) {
+				current = neighbor;
+			} else {
+				//If the neighbor has larger or equal residue, then with probability e-T, make it the current subset.
+				long probability = (getResidue(neighbor) - getResidue(current)) / (long)(Math.pow(10,11) * Math.pow(0.8,i/300));
+				//System.out.print("prob:" + probability);
+				if (probability >= 0.05) {
+					current = neighbor;
+				}
+			}
+		}
+
+		return smallest_r;
+	}
+
+
+
+
+	public static void main(String args[]){
+		Annealing annealing = new Annealing();
+		long[] arr = annealing.generateRandomList(100);
+        System.out.println("Random List:");
+        annealing.printList(arr);
+
+        //test random subset
+        System.out.println("\nRandom Subset:");
+        long[] sub = annealing.generateRandomSubset(arr);
+        annealing.printList(sub);
+
+        long[] neighbor = annealing.generateRandomNeighbor(arr, sub);
+        annealing.printList(neighbor);
+
+        System.out.println("\nTest residue:");
+        long residue = annealing.getResidue(sub);
+        System.out.print(residue);
+
+        System.out.println("\nTest smallest residue:");
+        long s_residue = annealing.getSmallestResidue(arr, sub, 100);
+        System.out.print("\nresult:" + s_residue + "\n");
+
+        long residue2 = Annealing(arr, 100);
+        System.out.print("\nresult2:" + residue2);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/Greedy.java
@@ -0,0 +1,172 @@
+//Implement the Greedy Algorithm
+//Zilin Chen
+//4/23/2018
+
+
+import java.lang.Math;
+
+public class Greedy {
+
+	//Instructor
+	public Greedy() {
+		System.out.println("This is the Greedy Algorithm: \n");
+	}
+
+	//takes a random long array
+	public static long Greedy(long[] randomArr){
+		long[] subset = new long[randomArr.length];
+		long[] sortedArr = sort(randomArr, 0, randomArr.length-1);
+		long residue = findResidue(sortedArr);
+		return residue;
+	}
+
+	//generate list of 100 random numbers from 1 to 10^12
+	public static long[] generateRandomList(int num){
+		long[] list = new long[num];
+		long leftLimit = 1;
+		long rightLimit = (long) Math.pow(10,12);
+		// System.out.print(leftLimit+(rightLimit-leftLimit)*Math.random());
+		for (int i=0; i<num ; i++) {
+			list[i] = (long)(leftLimit + Math.random()*(rightLimit-leftLimit));
+			// System.out.print(list[i]+"\n");
+		}
+		return list;
+	}
+
+	//print out the list
+	public static void printList(long[] list){
+		for (int i=0; i<list.length ;i++ ) {
+			System.out.print(i + ": " + list[i] + "\n");
+		}
+		System.out.println();
+	}
+
+	//merge sort for long list from largest to smallest
+	public static void merge(long arr[], int l, int m, int r){
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        /* Create temp arrays */
+        long L[] = new long [n1];
+        long R[] = new long [n2];
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
+ 
+        /* Merge the temp arrays */
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2){
+            if (L[i] >= R[j]){
+                arr[k] = L[i];
+                i++;
+            }else{
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        /* Copy remaining elements of L[] if any */
+        while (i < n1){
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        /* Copy remaining elements of R[] if any */
+        while (j < n2){
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+ 
+   //main sort function
+    public static long[] sort(long[] arr, int l, int r){
+        if (l < r) {
+            // Find the middle point
+            int m = (l+r)/2;
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr , m+1, r);
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+        return arr;
+    }
+
+    public static long findSum(long[] arr){
+    	long sum = 0;
+    	for (int i=0; i<arr.length; i++ ) {
+    		sum += arr[i];
+    	}
+    	return sum;
+    }
+ 	
+	//find residue
+    public static long findResidue(long[] arr){
+    	long target = (long)(25*Math.pow(10,12));
+    	long[] subset = new long[arr.length];
+    	//repeatedly adding elements to the subset
+    	for (int i=0; i<arr.length ; i++) {
+    		//If the integer will not give the subset too large a sum, add it to the subset.
+    		long sum = findSum(subset) + arr[i];
+    		if (sum <= target) {
+    			subset[i] = arr[i];
+    		}
+    	}
+
+    	//System.out.println("\nSublist List:");
+    	//printList(subset);
+
+    	long residue = target - findSum(subset);
+    	return residue;
+    }
+
+
+
+    //test function
+	public static void main(String args[]){
+        
+        System.out.print(Math.random());
+        System.out.println();
+        
+        //test random list
+        Greedy greedy = new Greedy();
+        //step one: generate random list
+        // long[] list = greedy.generateRandomList(100);
+        //greedy.printList(list);
+
+        //test merge sort
+        long[] arr = greedy.generateRandomList(100);
+        System.out.println("Random List:");
+        greedy.printList(arr);
+        long[] sortedArr = greedy.sort(arr,0,arr.length-1);
+        System.out.println("Sorted List:");
+        greedy.printList(sortedArr);
+
+        //test find sum
+        System.out.println("Sum of Sorted List:");
+        System.out.print(greedy.findSum(sortedArr)+"\n");
+
+        //test finding subset
+        long residue = greedy.findResidue(sortedArr);
+        System.out.println("Residue:");
+        System.out.print(residue + "\n");
+
+        //test
+        long residue2 = Greedy(arr);
+        System.out.println("Residue2:");
+        System.out.print(residue2 + "\n");
+     
+    }
+
+
+
+
+
+
+}
